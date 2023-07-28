d2ec130902de36ec80e9396a89be20202cc0c597
Visitor <visitor@Small-Mac.local>
2023-07-28 15:26:28 +0300

    GraphQL

--- compose/start/app/src/main/java/com/example/rocketreserver/LaunchDetails.kt
+++ compose/start/app/src/main/java/com/example/rocketreserver/LaunchDetails.kt
@@ -1,6 +1,5 @@
 package com.example.rocketreserver
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,22 +14,56 @@ import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import coil.compose.AsyncImage
+import com.apollographql.apollo3.api.Error
+import com.apollographql.apollo3.exception.ApolloException
 
 @Composable
 fun LaunchDetails(launchId: String) {
+    var state by remember { mutableStateOf<LaunchDetailsState>(LaunchDetailsState.Loading) }
+    LaunchedEffect(Unit){
+        state = try {
+            val response = apolloClient.query(LaunchQuery(launchId)).execute()
+            if (response.hasErrors()) {
+                LaunchDetailsState.ApplicationError(response.errors!!)
+            } else {
+                LaunchDetailsState.Success(response.data!!)
+            }
+        }catch (e: ApolloException) {
+            LaunchDetailsState.ProtocolError(e)
+        }
+    }
+    // Use the state
+    when (val s = state) {
+        is LaunchDetailsState.Loading -> Loading()
+        is LaunchDetailsState.ProtocolError -> ErrorMessage("Oh no... A protocol error happened: ${s.exception.message}")
+        is LaunchDetailsState.Success -> LaunchedDetails(s.data)
+        is LaunchDetailsState.ApplicationError -> ErrorMessage(text = s.errors.toString())
+    }
+}
+
+@Composable
+private fun LaunchedDetails(response: LaunchQuery.Data) {
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             // Mission patch
-            Image(
+            AsyncImage(
                 modifier = Modifier.size(160.dp, 160.dp),
-                painter = painterResource(R.drawable.ic_placeholder),
+                model = response?.launch?.mission?.missionPatch,
+                placeholder = painterResource(R.drawable.ic_placeholder),
+                error = painterResource(R.drawable.ic_placeholder),
                 contentDescription = "Mission patch"
             )
 
@@ -40,21 +73,21 @@ fun LaunchDetails(launchId: String) {
                 // Mission name
                 Text(
                     style = MaterialTheme.typography.headlineMedium,
-                    text = "Launch $launchId"
+                    text = response?.launch?.mission?.name ?: ""
                 )
 
                 // Rocket name
                 Text(
                     modifier = Modifier.padding(top = 8.dp),
                     style = MaterialTheme.typography.headlineSmall,
-                    text = "Rocket name",
+                    text = response?.launch?.rocket?.name?.let { "\uD83D\uDE80 $it" } ?: "",
                 )
 
                 // Site
                 Text(
                     modifier = Modifier.padding(top = 8.dp),
                     style = MaterialTheme.typography.titleMedium,
-                    text = "Site..."
+                    text = response?.launch?.site ?: ""
                 )
             }
         }
@@ -98,4 +131,13 @@ private fun SmallLoading() {
 @Composable
 private fun LaunchDetailsPreview() {
     LaunchDetails(launchId = "42")
+}
+
+
+private sealed interface LaunchDetailsState {
+    object Loading : LaunchDetailsState
+    data class ProtocolError(val exception: ApolloException) : LaunchDetailsState
+    data class Success(val data: LaunchQuery.Data) : LaunchDetailsState
+    data class ApplicationError(val errors: List<Error>) : LaunchDetailsState
+
 }
--- compose/start/app/src/main/java/com/example/rocketreserver/LaunchList.kt
+++ compose/start/app/src/main/java/com/example/rocketreserver/LaunchList.kt
@@ -2,49 +2,77 @@
 
 package com.example.rocketreserver
 
-import androidx.compose.foundation.Image
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.apollographql.apollo3.api.ApolloResponse
+import com.apollographql.apollo3.api.Optional
 
 @Composable
 fun LaunchList(onLaunchClick: (launchId: String) -> Unit) {
+    var launchlist by remember{ mutableStateOf(emptyList<LaunchesQuery.Launch>()) }
+    var cursor: String? by remember { mutableStateOf(null) }
+    var response: ApolloResponse<LaunchesQuery.Data>? by remember { mutableStateOf(null) }
+
+    LaunchedEffect(cursor){
+        response = apolloClient.query(LaunchesQuery(Optional.present(cursor))).execute()
+        Log.d("LAUNCHES","SUCCESS  ${response?.data}")
+        launchlist = launchlist + response?.data?.launches?.launches?.filterNotNull().orEmpty()
+    }
+
     LazyColumn {
-        items(20) {
-            LaunchItem(launchId = it.toString(), onClick = onLaunchClick)
+        items(launchlist) { launch ->
+            LaunchItem(launch = launch, onClick = onLaunchClick)
+        }
+        item{
+            if(response?.data?.launches?.hasMore == true){
+                LoadingItem()
+                cursor = response?.data?.launches?.cursor
+            }
         }
     }
 }
 
 @Composable
-private fun LaunchItem(launchId: String, onClick: (launchId: String) -> Unit) {
+private fun LaunchItem(launch:LaunchesQuery.Launch, onClick: (launchId: String) -> Unit) {
     ListItem(
-        modifier = Modifier.clickable { onClick(launchId) },
+        modifier = Modifier.clickable { onClick(launch.id) },
         headlineText = {
             // Mission name
-            Text(text = "Launch $launchId")
+            Text(text = launch.mission?.name ?: "")
         },
         supportingText = {
             // Site
-            Text(text = "Site...")
+            Text(text = launch.site?:"")
         },
         leadingContent = {
             // Mission patch
-            Image(
+            AsyncImage(
                 modifier = Modifier.size(68.dp, 68.dp),
-                painter = painterResource(R.drawable.ic_placeholder),
+                model = launch.mission?.missionPatch,
+                placeholder = painterResource(R.drawable.ic_placeholder),
+                error = painterResource(R.drawable.ic_placeholder),
                 contentDescription = "Mission patch"
             )
         }
@@ -61,4 +89,13 @@ private fun LoadingItem() {
     ) {
         CircularProgressIndicator()
     }
+}
+
+
+@Preview
+@Composable
+fun LaunchesPreview() {
+    var launchlist by remember { mutableStateOf(emptyList<LaunchesQuery.Launch>()) }
+
+    LaunchList(onLaunchClick = {})
 }
